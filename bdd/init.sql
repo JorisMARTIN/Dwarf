00c7ff7679f6c479d3dfa578c28b97f92234df44
Powerock38 <mat.papp@free.fr>
2020-11-18 12:00:41 +0100

    changed placeholder user password

--- bdd/init.sql
+++ bdd/init.sql
@@ -1,6 +1,6 @@
 \i drop.sql
 \i create.sql
 
-INSERT INTO "User" (nickname, email, password, creationDate, IPs) VALUES ('Matteo', 'matteo@gmail.com', '$2y$10$BuIzjzkW4AJj6DAbuy7e/Ol3kZ8xZlAfPlUlDyA4Mdh1X3qxEn966', '2020-08-10', '{"192.168.0.1", "200.200.200.200"}');
+INSERT INTO "User" (nickname, email, password, creationDate, IPs) VALUES ('Matteo', 'matteo@gmail.com', '$2y$10$bPBV0ZNbZ9.AsVBMfl46DewzBqo5f5xcGt6ussY2YXgxtlg2LhLqy', '2020-08-10', '{"192.168.0.1", "200.200.200.200"}');
 INSERT INTO "Page" (name, description, gameMode, template, creationDate, userId) VALUES ('Test', 'Ceci est un test', 1, 0, '2020-08-10', 1);
 INSERT INTO "Frame" (width, height, imagePtr, creationDate, pageId) VALUES (10, 10, '', '2020-08-10', 1);
