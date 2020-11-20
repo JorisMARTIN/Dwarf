b7d0d6f4421778e111f25813b0553ec1549c91f0
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2020-11-20 17:28:00 +0100

    CSS sur home

--- dwarf-browser/src/Home/index.js
+++ dwarf-browser/src/Home/index.js
@@ -4,9 +4,21 @@ import Auth from '../components/AuthHelperMethods';
 
 function ComicPage(props) {
     return (
-        <div>
-            <h3>{props.name}</h3>
-            <p>{props.desc}</p>
+        <div className="homePlanche">
+            <div className="homePlancheTop">
+                <img className="homePlancheImg" src={props.name} alt={props.name}></img>
+                <div>
+                    <h3 className="homeName">Titre : {props.name}</h3>
+                    <p className="homeDescri">Description :<br></br>
+                        {props.desc}</p>
+                    <p className="homeMode">Mode :<br></br>
+                        {props.mode}</p>
+                </div>
+            </div>
+            <div className="homePlancheBottom">
+                <button type="button">Like</button> 
+                <button type="button">Dislike</button> 
+            </div>
         </div>
     );
 }
@@ -21,7 +33,7 @@ export default class Home extends Component {
             .then(res => {
                 this.setState({
                     pages: res.pages.map((page, i) =>
-                        <ComicPage key={i} name={page.name} desc={page.description} />
+                        <ComicPage key={i} name={page.name} desc={page.description} mode={page.gamemode}/>
                     )
                 });
             })
@@ -30,8 +42,12 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
-                {this.state.pages}
+                <h1 className="homeTitle">Home</h1>
+                <div className="homeMain">
+                    <div className="homeDivPlanche">
+                        {this.state.pages}
+                    </div>
+                </div>
             </div>
         );
     }
