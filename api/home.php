7acf85905d26b20469399980dc23b15a072191c0
jhistart <thomas.jhistarry@etu.univ-grenoble-alpes.fr>
2020-11-21 19:31:46 +0100

    Juste un peu de mise au propre de code

--- api/home.php
+++ api/home.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__) . '/includes/httpheaders.inc.php');
 
-require_once(dirname(__FILE__).'/model/PageDAO.class.php');
+require_once(dirname(__FILE__) . '/model/PageDAO.class.php');
 require_once(dirname(__FILE__) . '/includes/debug.inc.php');
 // Renvoie une liste de BDs
 
@@ -11,23 +11,26 @@ $data = json_decode(file_get_contents("php://input"));
 
 $lastId = ($data->lastPageLoadedId == -1) ? $pageDAO->getLastPageId() : $data->lastPageLoadedId;
 
-if($data->lastPageLoadedId == -1) $nextId = $lastId;
-else $nextId = ($lastId - 1 <= 1) ? 1 : $lastId - 1;
+if ($data->lastPageLoadedId == -1) {
+    $nextId = $lastId;
+} else {
+    $nextId = ($lastId - 1 <= 1) ? 1 : $lastId - 1;
+}
 
 $willReachEnd = $nextId - 5 < 1;
 
-if($willReachEnd){
+if ($willReachEnd) {
     $numberToLoad = 0;
-    while($nextId - $numberToLoad >= 2){
+    while ($nextId - $numberToLoad >= 2) {
         $numberToLoad++;
     }
-}else{
+} else {
     $numberToLoad = 5;
 }
 
-if($nextId != 1){
+if ($nextId != 1) {
     $pages = $pageDAO->getRangeOfPages($nextId - $numberToLoad, $nextId);
-}else{
+} else {
     $page = [];
 }
 
