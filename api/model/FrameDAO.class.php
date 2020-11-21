a6b76007e0b13a535ccea79a7bf4b8f8c9d5ce7c
dwarf <dwarf@vps-83b0a320.vps.ovh.net>
2020-11-21 13:35:21 +0000

    Correction du path des frames

--- api/model/FrameDAO.class.php
+++ api/model/FrameDAO.class.php
@@ -38,7 +38,7 @@ class FrameDAO extends DAO {
       ':userId' => $userId
     ])) {
       $frameId = $tmp->fetchColumn();
-      $this->setImagePtr($frameId, dirname(__FILE__)."/frame-".$frameId);
+      $this->setImagePtr($frameId, dirname(__FILE__)."/../../cdn/frames/frame-".$frameId);
       return $frameId;
     } else {
       var_dump($tmp->errorInfo());
