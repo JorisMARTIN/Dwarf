e37854be918badd28a24cdaeb0672f2ad6ce7546
dwarf <dwarf@vps-83b0a320.vps.ovh.net>
2020-11-21 11:39:45 +0000

    dao test, thomas tu peux commit stp que je n'ai pas à le faire à ta place merci bisous

--- api/test/DAO.test.php
+++ api/test/DAO.test.php
@@ -10,24 +10,63 @@ $pageDAO = new PageDAO();
 $frameDAO = new FrameDAO();
 
 $user1 = $userDAO->getUser(1);
+print("Récupération d'un utilisateur : ");
 if ($user1) {
-  print("Récupération d'un utilisateur : OK");
+  print("OK");
   $user1Id = $user1->getId();
-  print(" - Vérification de son identifiant : ".($user1Id === 1 ? "OK" : "FAILED"));
+  print("\n - Vérification de son identifiant : ".($user1Id === 1 ? "OK" : "FAILED"));
   $user1NickName = $user1->getNickName();
-  print(" - Vérification de son pseudo : ".($user1NickName === "dwarf" ? "OK" : "FAILED"));
+  print("\n - Vérification de son pseudo : ".($user1NickName === "dwarf" ? "OK" : "FAILED"));
   $user1Email = $user1->getEmail();
-  print("Vérification de son email : ".($user1Email === "dwarf@gmail.com" ? "OK" : "FAILED"));
+  print("\n - Vérification de son email : ".($user1Email === "dwarf@gmail.com" ? "OK" : "FAILED"));
+  $user1CreationDate = $user1->getCreationDate();
+  print("\n - Vérification de sa date de création : ".($user1CreationDate > "2000-01-01" ? "OK" : "FAILED"));
+} else {
+  print("FAILED");
+}
+print("\n\n");
 
-$page1 = $pageDAO->getUserPages($user1Id)[0];
 $page2 = $pageDAO->getPage(2);
-$frame1 = $frameDAO->getFrames($page1->getId());
+print("Récupération d'une planche : ");
+if ($page2) {
+  print("OK");
+} else {
+  print("FAILED");
+}
+print("\n");
+$page1 = $pageDAO->getUserPages($user1Id)[0];
+print("Récupération d'une planche depuis un utilisateur : ");
+if ($page1) {
+  print("OK");
+  $page1Id = $page1->getId();
+  print("\n - Vérification de son identifiant : ".($page1Id === 1 ? "OK" : "FAILED"));
+  $page1Name = $page1->getName();
+  print("\n - Vérification de son nom : ".($page1Name === "Page 1" ? "OK" : "FAILED"));
+  $page1Description = $page1->getDescription();
+  print("\n - Vérification de sa description : ".($page1Description === "Ceci est la page 1" ? "OK" : "FAILED"));
+  $page1CreationDate = $page1->getCreationDate();
+  print("\n - Vérification de sa date de création : ".($page1CreationDate > "2000-01-01" ? "OK" : "FAILED"));
+  $page1Gamemode = $page1->getGameMode();
+  print("\n - Vérification de son mode de jeu : ".($page1Name >= 0 ? "OK" : "FAILED"));
+} else {
+  print("FAILED");
+}
+print("\n\n");
 
-var_dump($page1);
-var_dump($page2);
-var_dump($frame1);
+$frame1 = $frameDAO->getFrames($page1Id);
+print("Récupération d'une frame : ");
+if ($frame1) {
+  print("OK");
+} else {
+  print("FAILED");
+}
+print("\n\n");
 
-var_dump($pageDAO->putPage('Creation', 'Je suis', 0, 0, false, 1));
-var_dump($frameDAO->putFrame(True, False, 100, 100, 1, 1));
+$newUserId = $userDAO->putUser('DrStone@gmail.com', 'drStone', 'qwerty', '192.168.1.1');
+print("Création d'un utilisateur : ".($newUserId ? "OK" : "FAILED ($newUserId)")."\n");
+$newPageId = $pageDAO->putPage('Creation de DrStone', 'Téléphone', 0, 0, false, 1);
+print("Création d'une planche : ".($newPageId != -1 ? "OK" : "FAILED ($newPageId)")."\n");
+$newFrameId = $frameDAO->putFrame(True, False, 100, 100, $newPageId, 1);
+print("Création d'une frame : ".($newFrameId != -1 ? "OK" : "FAILED ($newFrameId)")."\n");
 
 ?>
