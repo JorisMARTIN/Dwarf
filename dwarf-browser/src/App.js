bb7b8be46e270cceb0f0963313428ef2b55f09ec
Joris Martin <joris.martin@etu.univ-grenoble-alpes.fr>
2020-11-21 17:47:53 +0100

    Logout button

--- dwarf-browser/src/App.js
+++ dwarf-browser/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,53 +12,67 @@ import InitDrawing from './InitDrawing';
 import AuthPage from './AuthPage';
 import E404 from './E404';
 import Canvas from './Canvas';
+import Auth from './components/AuthHelperMethods';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link className="link" to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="link" to="/draw">Draw !</Link>
-          </li>
-          <li>
-            <Link className="link" to="/init">Initiate a drawing</Link>
-          </li>
-          <li>
-            <Link className="link" to="/auth">Log in / sign up</Link>
-          </li>
-        </ul>
-      </div>
+export default class App extends Component {
 
+  state = {
+    logged: Auth.loggedIn,
+  };
 
-      <hr />
+  logout = (e) => {
+    e.preventDefault();
+    this.setState({logged: false});
+    Auth.logout();
+  };
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+  render() {
+    return (
 
-        <Route path="/draw">
-          <Draw />
-        </Route>
+      <Router>
+        <div className="appMenu">
+          <ul>
+            <li>
+              <Link className="link" to="/">Home</Link>
+            </li>
+            <li>
+              <Link className="link" to="/draw">Draw !</Link>
+            </li>
+            <li>
+              <Link className="link" to="/init">Initiate a drawing</Link>
+            </li>
+            <li>
+              <Link className="link" to="/auth">Log in / sign up</Link>
+            </li>
+          </ul>
 
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
+          {this.state.logged && <button className="logout" onClick={this.logout}>Log out</button>}
+        </div>
 
-        <Route path="/init">
-          <InitDrawing />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route path={"/canvas/:id"} component={Canvas}/>
+          <Route path="/draw">
+            <Draw />
+          </Route>
 
-        <Route path="*">
-          <E404 />
-        </Route>
-      </Switch>
-    </Router>
-  );
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+
+          <Route path="/init">
+            <InitDrawing />
+          </Route>
+
+          <Route path={"/canvas/:id"} component={Canvas}/>
+
+          <Route path="*">
+            <E404 />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
