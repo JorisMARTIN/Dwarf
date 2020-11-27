3eec75ec6386cc15be919234dc203bddfb0affba
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2020-11-27 17:49:54 +0100

    Test erreur

--- api/signup.php
+++ api/signup.php
@@ -3,6 +3,7 @@ require_once(dirname(__FILE__) . '/includes/httpheaders.inc.php');
 
 require_once(dirname(__FILE__) . '/model/AuthMethods.php');
 require_once(dirname(__FILE__) . '/model/UserDAO.class.php');
+require_once(dirname(__FILE__) . '/includes/debug.inc.php');
 
 $userDAO = new UserDAO();
 
