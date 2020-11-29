20997787f0a3c2f503bb1c6ccbe25eb3eb5cbaa1
Powerock38 <mat.papp@free.fr>
2020-11-29 17:01:29 +0100

    selector.php réécris dsl thomas

--- api/selector.php
+++ api/selector.php
@@ -1,9 +1,9 @@
 <?php
+require_once(dirname(__FILE__) . '/includes/debug.inc.php');
 require_once(dirname(__FILE__) . '/includes/httpheaders.inc.php');
 
 require_once(dirname(__FILE__) . '/model/PageDAO.class.php');
 require_once(dirname(__FILE__) . '/model/FrameDAO.class.php');
-require_once(dirname(__FILE__) . '/includes/debug.inc.php');
 // Renvoie une liste de BDs
 
 $pageDAO = new PageDAO();
@@ -11,34 +11,47 @@ $frameDAO = new FrameDAO();
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(!empty($data)) {
+if (!empty($data)) {
     $loadedIds = $data->loadedIds;
     $direction = $data->direction;
-    $length = count($loadedIds);
+    $index = $data->index;
 
-    if ($length == 0) {
-        $index = 0;
-        $page = $pageDAO->getRandomPage();
-        $id = $page ? $page->getId() : -1;
-        $loadedIds = array($id);
+    $i = $index + $direction;
+
+    if (isset($loadedIds[$i])) {
+        $page = $pageDAO->getPage($loadedIds[$i]);
     } else {
-        $index = $data->index;
-        if ($direction == -1 && $index <= 0) {
-            $index = 0;
-            $page = $pageDAO->getRandomPage();
-            $id = $page ? $page->getId() : -1;
-            array_unshift($loadedIds, $id);
-        } else if ($direction == 1 && $index >= $length) {
-            $index = $length;
-            $page = $pageDAO->getRandomPage();
-            $id = $page ? $page->getId() : -1;
-            array_push($loadedIds, $id);
-        } else {
-            $index += $direction;
-            $id = $loadedIds[$index];
-            $page = $pageDAO->getPage($id);
-        }
+        $page = $pageDAO->getRandomPage($loadedIds);
+        if ($page != NULL) {
+            $loadedIds[$i] = $page->getId();
+            $loadedIds = array_values($loadedIds);
+        } // ptet gérer le fait que yai plus rien de nouveau à scroller
     }
+
+
+    // if (empty($loadedIds)) {
+    //     $index = 0;
+    //     $page = $pageDAO->getRandomPage();
+    //     $id = $page ? $page->getId() : -1;
+    //     $loadedIds = array($id);
+    // } else {
+
+    //     if ($direction == -1 && $index <= 0) {
+    //         $index = 0;
+    //         $page = $pageDAO->getRandomPage();
+    //         $id = $page ? $page->getId() : -1;
+    //         array_unshift($loadedIds, $id);
+    //     } else if ($direction == 1 && $index >= $length) {
+    //         $index = $length;
+    //         $page = $pageDAO->getRandomPage();
+    //         $id = $page ? $page->getId() : -1;
+    //         array_push($loadedIds, $id);
+    //     } else {
+    //         $index += $direction;
+    //         $id = $loadedIds[$index];
+    //         $page = $pageDAO->getPage($id);
+    //     }
+    // }
 
     $out = [
         'loadedIds' => $loadedIds,
@@ -46,6 +59,8 @@ if(!empty($data)) {
         'page' => NULL
     ];
 
+    //todo : envoyer imagePtr de l'image de référence de l'image à dessiner
+    // et le frameId / frameWidth / frameHeight de l'image à dessiner
     if ($page) {
         $out['page'] = [
             'name' => $page->getName(),
@@ -55,8 +70,6 @@ if(!empty($data)) {
             'imagePtr' => $frameDAO->getFrames($page->getId())[0]->getImagePtr()
         ];
     }
-
-
 } else {
     $out = [];
 }
