e963614a54e4447766cd7a85225f3875a37f583b
Ayache Inzouddine <ayache.inzouddine@etu.univ-grenoble-alpes.fr>
2020-12-04 16:11:20 +0100

    correction

--- api/model/RateDAO.class.php
+++ api/model/RateDAO.class.php
@@ -1,37 +1,41 @@
 <?php
 require_once(dirname(__FILE__) . '/DAO.class.php');
-require_once(dirname(__FILE__).'/Page.class.php');
+require_once(dirname(__FILE__) . '/Page.class.php');
 
-class RateDAO extends DAO {
+class RateDAO extends DAO
+{
 
     // Fonction retournant le nombre de votes positifs et négatifs pour une page donnée :
-    function getVote(int $pageId) : int { 
+    function getVote(int $pageId): int
+    {
         $query = 'SELECT count(userId) FROM "Rate" WHERE pageId = :pageId';
+        $tmp = $this->db->prepare($query);
         if ($tmp->execute()) {
             $res = $tmp->fetchColumn();
-            if(!$res) return -1;
+            if (!$res) return -1;
             else return $res;
         } else {
-            return -1; 
+            return -1;
         }
-
     }
-    
+
     // Fonction retournant pour un id utilisateur donné les pages id où il a voté :
-    function getUserVote(int $userId) : array {
+    function getUserVote(int $userId): array
+    {
         $query = 'SELECT * FROM "Page", "Rate" WHERE "Page".pageId = "Rate".pageId and "Rate".userId = :userId';
         $tmp = $this->db->prepare($query);
-            if ($tmp->execute([':userId' => $userId])) {
-                 return $tmp->fetchAll(PDO::FETCH_CLASS, 'Page');
-                } else {
-                 return NULL;
-             }
+        if ($tmp->execute([':userId' => $userId])) {
+            return $tmp->fetchAll(PDO::FETCH_CLASS, 'Page');
+        } else {
+            return NULL;
+        }
     }
-    
-    
+
+
     // Fonction enregistrant le vote d'un utilisateur dans la base de donnée
     // bool $vote représente le statut du vote (like ou dislike)
-    function putVote(int $userId, int $pageId, bool $vote) : bool { 
+    function putVote(int $userId, int $pageId, bool $vote): bool
+    {
         $query = 'INSERT INTO "Rate" (userId, pageId, vote)
                   VALUES (:userId, :pageId, :vote)';
 
@@ -39,18 +43,15 @@ class RateDAO extends DAO {
             ':userId' => $userId,
             ':pageId' => $pageId,
             ':vote'   => $vote ? 't' : 'f'
-          ]);
-    }
-    
-    function removeVote(int $pageId, int $userId) : bool {
-        $query = 'DELETE FROM "Rate" WHERE pageId = :pageId and userId = :userId';
-        return $this->db->prepare($query)->execute([
-        ':pageId' => $pageId,
-        ':userId' => $userId
         ]);
     }
-    
 
+    function removeVote(int $pageId, int $userId): bool
+    {
+        $query = 'DELETE FROM "Rate" WHERE pageId = :pageId and userId = :userId';
+        return $this->db->prepare($query)->execute([
+            ':pageId' => $pageId,
+            ':userId' => $userId
+        ]);
+    }
 }
-
-?>
