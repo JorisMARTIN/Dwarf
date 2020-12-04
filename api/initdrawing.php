bc41a8111fa2242dd95a8b4c5571aa4efe3691e8
Powerock38 <mat.papp@free.fr>
2020-12-04 16:15:40 +0100

    fix lol

--- api/initdrawing.php
+++ api/initdrawing.php
@@ -34,7 +34,7 @@ if ($userId != -1) {
             $tmpSize = count($TEMPLATES[$template]);
             for ($i = 0; $i < $tmpSize; $i++) {
                 $box = $TEMPLATES[$template][$i];
-                $fid = $frameDAO->putFrame(false, false, $box['w'], $box['h'], $pageId, $userId);
+                $fid = $frameDAO->putFrame(false, false, $box->w, $box->h, $pageId, $userId);
                 if (($gamemode == 0 && $i == 0) || ($gamemode == 1 && $i = $tmpSize)) {
                     $frameId = $fid;
                 }
