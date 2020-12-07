4b3d65fdbd4472d21e7bded987e2034f851761ab
Joris Martin <joris.martin@etu.univ-grenoble-alpes.fr>
2020-12-07 21:50:04 +0100

    Mise en place systeme de vote (controleur a faire)

---
+++ api/rate.php
@@ -0,0 +1,29 @@
+<?php
+require_once(dirname(__FILE__) . '/includes/debug.inc.php');
+require_once(dirname(__FILE__) . '/includes/httpheaders.inc.php');
+
+require_once(dirname(__FILE__) . '/model/AuthMethods.php');
+require_once(dirname(__FILE__) . '/model/PageDAO.class.php');
+
+
+$userId = tokenToUserId();
+
+// User logged in
+if($userId != -1){
+    
+    $data = json_decode(file_get_contents("php://input"));
+    // voteType = 1 pour like et 0 pour dislike
+    $voteType = $data->voteType;
+
+    $out = [];
+
+}else{
+    // User not logged in
+    $out = [
+        'message' => "You have to be logged for rate a page !\nDo you want to Log In or Sign Up ?"
+    ];
+}
+
+echo json_encode($out);
+
+?>
