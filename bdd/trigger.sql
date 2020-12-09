633b7938664e95bee46c3f638b5fd57ccd00e507
Joris Martin <joris.martin@etu.univ-grenoble-alpes.fr>
2020-12-09 15:23:06 +0100

    trigger v4

--- bdd/trigger.sql
+++ bdd/trigger.sql
@@ -5,24 +5,13 @@ DECLARE
 
 BEGIN
 
-    if(TG_OP = 'INSERT') then
-        if (isDeleted) then
-            RAISE EXCEPTION '(t_setDeleteForPage) page n° % is already deleted', new.pageId;
-        else 
-            UPDATE "Page" SET deleted = true WHERE pageId = new.pageId;
-            RAISE NOTICE '(t_setDeleteForPage) Page n° % set deleted to true', new.pageId;
-        end if;
-    else
-        if(EXISTS (SELECT pageId FROM "DeletePage" WHERE pageId = new.pageId)) then
-            UPDATE "Page" SET deleted = true WHERE pageId = new.pageId;
-            RAISE NOTICE '(t_setDeleteForPage) Page n° % set deleted to false', new.pageId;
-
-            DELETE FROM "DeletePage" WHERE pageId = new.pageId;
-            RAISE NOTICE '(t_setDeleteForPage) Page n° % deleted from %', TG_OP, TG_TABLE_NAME;
-        else
-            RAISE EXCEPTION '(t_setDeleteForPage) Page n° % is not deleted.', new.pageId;
-        end if;
+    if (isDeleted) then
+        RAISE EXCEPTION '(t_setDeleteForPage) page n° % is already deleted', new.pageId;
+    else 
+        UPDATE "Page" SET deleted = true WHERE pageId = new.pageId;
+        RAISE NOTICE '(t_setDeleteForPage) Page n° % set deleted to true', new.pageId;
     end if;
+
     RAISE NOTICE '(t_setDeleteForPage) % in %', TG_OP, TG_TABLE_NAME;
     RETURN new;
 
@@ -31,7 +20,35 @@ END; $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER t_setDeleteForPage
 BEFORE
-INSERT or DELETE
+INSERT
 ON "DeletePage"
 FOR EACH ROW
 EXECUTE procedure f_setDeleteForPage();
+
+CREATE OR REPLACE function f_removeDeleteForPage() RETURNS trigger AS $$
+DECLARE
+
+    isInTable boolean := EXISTS (SELECT deleted FROM "DeletePage" WHERE pageId = old.pageId);
+
+BEGIN
+
+    if (isInTable) then
+        UPDATE "Page" SET deleted = false WHERE pageId = old.pageId;
+        RAISE NOTICE '(t_removeDeleteForPage) Page n° % set deleted to false', old.pageId;
+    else 
+        RAISE EXCEPTION '(t_removeDeleteForPage) page n° % is already deleted', new.pageId;
+    end if;
+
+    RAISE NOTICE '(t_removeDeleteForPage) % in %', TG_OP, TG_TABLE_NAME;
+    RETURN new;
+
+
+END; $$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER t_removeDeleteForPage
+BEFORE
+DELETE
+ON "DeletePage"
+FOR EACH ROW
+EXECUTE procedure f_removeDeleteForPage();
