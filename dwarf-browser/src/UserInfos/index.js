f6265cbf984f3bad6582fbdd6f52c584e8ca6fa7
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2021-01-13 14:41:51 +0100

    Integration des differents part as component...

--- dwarf-browser/src/UserInfos/index.js
+++ dwarf-browser/src/UserInfos/index.js
@@ -5,6 +5,114 @@ import withAuth from '../components/withAuth';
 import ComicPage from '../components/ComicPage/ComicPage.js';
 import ComicFrame from '../components/ComicFrame/ComicFrame.js';
 
+const UserInfoComponent = (messageError) => (
+    <div className="userForm">
+        {messageError && <p className="authPageMessage">Erreur : {messageError}</p>}
+        <div className="userPageInfosOthers">
+            <p>Email : {userInfos?.email}</p>
+            <p>Date de naissance : {userInfos?.birthdate}</p>
+            <p>Date de création : {userInfos?.creationDate}</p>
+        </div>
+        <div className="userPageInfosBottom">
+            <button onClick={() => this.setState({ modify: true })} className="userPageDeleteAccount">Modifier</button>
+            <button onClick={() => this.handleAccount("delete")} className="userPageDeleteAccount">Supprimer le compte</button>
+        </div>
+    </div>
+);
+
+const UserFormComponent = (messageError) => (
+    <div className="userForm">
+        {messageError && <p className="authPageMessage">Erreur : {messageError}</p>}
+        <div className="userFormEmail">
+            <label htmlFor="emailUser">E-mail :</label>
+            <input
+                required
+                id="emailUser"
+                placeholder="Email"
+                name="emailUser"
+                type="email"
+                onChange={this._handleChange}
+            />
+        </div>
+        <div className="userFormBirthdate">
+            <label htmlFor="dateUser">Date de naissance :</label>
+            <input
+                required
+                id="dateUser"
+                placeholder="dd-mm-yyyy"
+                name="dateUser"
+                type="text"
+                onChange={this._handleChange}
+            />
+        </div>
+        <div className="userFormPwd">
+            <label htmlFor="pwdUser">Mot de passe :</label>
+            <input
+                required
+                id="pwdUser"
+                placeholder="Mot de passe"
+                name="passwordUser"
+                type="password"
+                onChange={this._handleChange}
+            />
+        </div>
+        <div className="userFormPwdConfirm">
+            <label htmlFor="pwdConfirmUser">Confirmation du mot de passe :</label>
+            <input
+                required
+                id="pwdConfirmUser"
+                placeholder="Confirmation du mot de passe"
+                name="passwordConfirmUser"
+                type="password"
+                onChange={this._handleChange}
+            />
+        </div>
+        <div className="userPageInfosBottom">
+            <div>
+                <button onClick={() => this.setState({ modify: false })} className="userPageButton">Annuler</button>
+                <button onClick={() => this.handleAccount("save")} className="userPageButton">Enregistrer</button>
+            </div>
+            <button onClick={() => this.handleAccount("delete")} className="userPageButton">Supprimer le compte</button>
+        </div>
+    </div>
+);
+
+const UserPagesComponent = (pages) => (
+    <article className="userPageCreationsFinish">
+        <div className="userPageTop">
+            <h1 className="userPageInfosPagesDone">BD déjà réalisé :</h1>
+        </div>
+        {/* Afffichage des planches fini où l'utilisateur à participé */}
+        <div className="userPageCreationsFinishContainer">
+            {pages.length > 0 ?
+                pages.map((page, i) => (
+                    <ComicPage key={i} {...page} userIsAdmin={isAdmin} />
+                ))
+                :
+                <h2>Il n'y a aucune BD fini où vous avez participé ...</h2>
+            }
+        </div>
+    </article>
+);
+
+const UserFramesComponent = (frames) => (
+    <article className="userPageCreationsCurrent">
+        <div className="userPageTop">
+            <h1 className="userPageInfosPagesNotDone">BD en cours de réalisation :</h1>
+        </div>
+        {/* Affichage des planches en cours où l'utilisateur à participé */}
+        <div className="userPageCreationsFinishContainer">
+            {frames.length > 0 ?
+                frames.map((frame, i) => (
+                    <ComicFrame key={i} {...frame} />
+                ))
+                :
+                <h2>Vous n'avez aucun dessin dans une BD non finie ....</h2>
+            }
+        </div>
+    </article>
+);
+
 class UserInfo extends Component {
 
     state = {
@@ -71,7 +179,7 @@ class UserInfo extends Component {
     }
 
     render() {
-        const { pages, frames, isAdmin, userInfos } = this.state;
+        const {userInfos} = this.state;
         return (
             <div className="userPage">
                 <section className="userPageMenu">
@@ -87,118 +195,14 @@ class UserInfo extends Component {
                                 {isAdmin && <p className="userPageInfosTopIsadmin">Admin</p>}
                             </div>
                             {!this.state.modify ?
-                            <div className="userForm">
-                                {this.state.messageError && <p className="authPageMessage">Erreur : {this.state.messageError}</p>}
-                                <div className="userPageInfosOthers">
-                                    <p>Email : {userInfos?.email}</p>
-                                    <p>Date de naissance : {userInfos?.birthdate}</p>
-                                    <p>Date de création : {userInfos?.creationDate}</p>
-                                </div>
-                                <div className="userPageInfosBottom">
-                                    {!this.state.modify ?
-                                        <button onClick={() => this.setState({modify : true}) } className="userPageDeleteAccount">Modifier</button>
-                                    : 
-                                        <button onClick={() => this.handleAccount("save")} className="userPageDeleteAccount">Enregistrer</button>
-                                    }
-                                    <button onClick={() => this.handleAccount("delete")} className="userPageDeleteAccount">Supprimer le compte</button>
-                                </div>
-                            </div>
+                            <UserInfoComponent userInfos={userInfos} messageError={this.state.messageError}/>
                             :
-                            <div className="userForm">
-                                {this.state.messageError && <p className="authPageMessage">Erreur : {this.state.messageError}</p>}
-                                <div className="userFormEmail">
-                                    <label htmlFor="emailUser">E-mail :</label>
-                                    <input
-                                        required
-                                        id="emailUser"
-                                        placeholder="Email"
-                                        name="emailUser"
-                                        type="email"
-                                        onChange={this._handleChange}
-                                    />
-                                </div>
-                                <div className="userFormBirthdate">
-                                    <label htmlFor="dateUser">Date de naissance :</label>
-                                    <input
-                                        required
-                                        id="dateUser"
-                                        placeholder="dd-mm-yyyy"
-                                        name="dateUser"
-                                        type="text"
-                                        onChange={this._handleChange}
-                                    />
-                                </div>
-                                <div className="userFormPwd">
-                                    <label htmlFor="pwdUser">Mot de passe :</label>
-                                    <input
-                                        required
-                                        id="pwdUser"
-                                        placeholder="Mot de passe"
-                                        name="passwordUser"
-                                        type="password"
-                                        onChange={this._handleChange}
-                                    />
-                                </div>
-                                <div className="userFormPwdConfirm">
-                                    <label htmlFor="pwdConfirmUser">Confirmation du mot de passe :</label>
-                                    <input
-                                        required
-                                        id="pwdConfirmUser"
-                                        placeholder="Confirmation du mot de passe"
-                                        name="passwordConfirmUser"
-                                        type="password"
-                                        onChange={this._handleChange}
-                                    />
-                                </div>
-                                <div className="userPageInfosBottom">
-                                    {!this.state.modify ?
-                                        <button onClick={() => this.setState({ modify: true })} className="userPageButton">Modifier</button>
-                                        :
-                                        <div>
-                                            <button onClick={() => this.setState({ modify: false })} className="userPageButton">Annuler</button>
-                                            <button onClick={() => this.handleAccount("save")} className="userPageButton">Enregistrer</button>
-                                        </div>
-                                    }
-                                    <button onClick={() => this.handleAccount("delete")} className="userPageButton">Supprimer le compte</button>
-                                </div>
-                            </div>
+                            <UserFormComponent messageError={this.state.messageError} />
                             }
                         </article>
                     }
-                    {this.state.section === 1 &&
-                        <article className="userPageCreationsFinish">
-                            <div className="userPageTop">
-                                <h1 className="userPageInfosPagesDone">BD déjà réalisé :</h1>
-                            </div>
-                            {/* Afffichage des planches fini où l'utilisateur à participé */}
-                            <div className="userPageCreationsFinishContainer">
-                                {pages.length > 0 ?
-                                    pages.map((page, i) => (
-                                        <ComicPage key={i} {...page} userIsAdmin={isAdmin} />
-                                    ))
-                                    :
-                                    <h2>Il n'y a aucune BD fini où vous avez participé ...</h2>
-                                }
-                            </div>
-                        </article>
-                    }
-                    {this.state.section === 2 &&
-                        <article className="userPageCreationsCurrent">
-                            <div className="userPageTop">
-                                <h1 className="userPageInfosPagesNotDone">BD en cours de réalisation :</h1>
-                            </div>
-                            {/* Affichage des planches en cours où l'utilisateur à participé */}
-                            <div className="userPageCreationsFinishContainer">
-                                {frames.length > 0 ?
-                                    frames.map((frame, i) => (
-                                        <ComicFrame key={i} {...frame} />
-                                    ))
-                                    :
-                                    <h2>Vous n'avez aucun dessin dans une BD non finie ....</h2>
-                                }
-                            </div>
-                        </article>
-                    }
+                    {this.state.section === 1 && <UserPagesComponent isAdmin={this.state.isAdmin} pages={this.state.pages}/>}
+                    {this.state.section === 2 && <UserFramesComponent frames={this.state.frames} /> }
                 </section>
             </div>
         );
